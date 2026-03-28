844f191d31546fb2f590ba692670fc25a1286e6c
hekemi <vaonmax@gmail.com>
2026-03-28 12:10:41 +0300

    Добавил миграцию для модели Employee и обновил обработчик сигналов для создания ролей и прав доступа после миграций.

--- Laba4/employees/signals.py
+++ Laba4/employees/signals.py
@@ -1,25 +1,47 @@
 from django.apps import apps
 from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
+from .models import Employee
+
 
 @receiver(post_migrate)
-def create_roles(sender, **kwargs):
-    Employee = apps.get_model("employees", "Employee")
-    ct = Employee._meta.app_label, Employee._meta.model_name
+def create_roles(sender, app_config=None, **kwargs):
+    # Выполняем только после миграций приложения employees
+    if app_config is None or app_config.label != "employees":
+        return
 
+    ct = ContentType.objects.get_for_model(Employee)
     perms = {
-        "view": Permission.objects.get_by_natural_key("view_employee", *ct),
-        "add": Permission.objects.get_by_natural_key("add_employee", *ct),
-        "change": Permission.objects.get_by_natural_key("change_employee", *ct),
-        "delete": Permission.objects.get_by_natural_key("delete_employee", *ct),
+        p.codename: p
+        for p in Permission.objects.filter(
+            content_type=ct,
+            codename__in=[
+                "view_employee",
+                "add_employee",
+                "change_employee",
+                "delete_employee",
+            ],
+        )
     }
+
+    required = {"view_employee", "add_employee", "change_employee", "delete_employee"}
+    if not required.issubset(perms.keys()):
+        return
 
     director, _ = Group.objects.get_or_create(name="Директор")
     deputy, _ = Group.objects.get_or_create(name="Заместитель директора")
     secretary, _ = Group.objects.get_or_create(name="Секретарь")
 
-    director.permissions.set([perms["view"], perms["add"], perms["change"], perms["delete"]])
-    deputy.permissions.set([perms["view"], perms["change"]])
-    secretary.permissions.set([perms["view"]])
+    director.permissions.set(
+        [
+            perms["view_employee"],
+            perms["add_employee"],
+            perms["change_employee"],
+            perms["delete_employee"],
+        ]
+    )
+    deputy.permissions.set([perms["view_employee"], perms["change_employee"]])
+    secretary.permissions.set([perms["view_employee"]])
